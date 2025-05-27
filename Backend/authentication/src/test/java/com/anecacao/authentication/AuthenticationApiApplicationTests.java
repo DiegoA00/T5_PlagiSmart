84fc789d29707ff0ef5da1de4b1beb4f30020c16
isaiasgh <joguherr@espol.edu.ec>
2025-05-26 22:03:07 -0500

    chore: initialize auth microservice project with Spring Initializr

---
+++ Backend/authentication/src/test/java/com/anecacao/authentication/AuthenticationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anecacao.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
