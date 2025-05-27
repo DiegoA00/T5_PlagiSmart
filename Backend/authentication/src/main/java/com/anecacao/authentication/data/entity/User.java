6d25408628de5f46d5925dbe57359b02bc00ac75
Izaako04 <isaac.criollo04@gmail.com>
2025-05-26 22:52:24 -0500

    feat(auth): Create user entity to storage in database

---
+++ Backend/authentication/src/main/java/com/anecacao/authentication/data/entity/User.java
@@ -0,0 +1,29 @@
+package com.anecacao.authentication.data.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "users_anecacao")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "national_id")
+    private String nationalId;
+
+    private String email;
+    private String location;
+    private LocalDateTime birthday;
+    private String rol;
+
+}
