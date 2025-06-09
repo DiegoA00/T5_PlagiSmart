ffa326980653e89898524a118ef8abadfa6f5a45
aszurita <nzuritag@fiec.espol.edu.ec>
2025-06-09 12:55:45 -0500

    Form to book a new appointment

--- Frontend/web/src/pages/Home/Components/NuevaReservaButton.jsx
+++ Frontend/web/src/pages/Home/Components/NuevaReservaButton.jsx
@@ -4,7 +4,7 @@ function NuevaReservaButton({ onClick }) {
   return (
     <button
       onClick={onClick}
-      className="fixed top-20 right-10 bg-blueAnecacao text-white px-6 py-3 rounded-full shadow-lg hover:bg-blue-700 transition"
+      className="bg-blueAnecacao text-white px-6 py-3 rounded-full shadow-lg hover:bg-blue-700 transition"
     >
       + Nueva Reserva
     </button>
--- Frontend/web/src/pages/Home/HomeCliente.jsx
+++ Frontend/web/src/pages/Home/HomeCliente.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReservationsList from './Components/ReservationsList';
 import NuevaReservaButton from './Components/NuevaReservaButton';
+import NewReservationForm from './Components/NewReservationForm';
 
 const reservasProximas = [
   { id: '0000001', day: 'LUNES', date: 18, month: 'Noviembre', time: '09:00', period: 'AM', status: 'Próxima', documents: [] },
@@ -11,18 +12,31 @@ const reservasEnCurso = [
 ];
 
 function HomeClient() {
+  const [showForm, setShowForm] = useState(false);
+
+  const handleNewReservationClick = () => {
+    setShowForm(true);
+  };
+
+  const handleCloseForm = () => {
+    setShowForm(false);
+  };
+
   return (
     <div className="flex flex-col">
-      <NuevaReservaButton onClick={() => alert('Crear nueva reserva')} />
         <div className='flex w-full'>
           <div className='h-screen w-1/12 min-w-[120px] bg-blueAnecacao pr-2'>
           </div>
-          <section className='h-full w-11/12 pt-10 px-2'>
+          <section className='h-full w-11/12 pt-2 px-2'>
+            <div className='flex justify-end place-content-center'>
+              <NuevaReservaButton onClick={handleNewReservationClick} />
+            </div>
             <ReservationsList title="Próximas" reservations={reservasProximas} />
             <ReservationsList title="En Curso" reservations={reservasEnCurso} />
           </section>
         </div>
 
+      {showForm && <NewReservationForm onClose={handleCloseForm} />}
     </div>
   );
 }
