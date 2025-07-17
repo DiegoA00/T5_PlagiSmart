f22092a7ce4360108418978ffe90a5225e80c155
isaiasgh <joguherr@espol.edu.ec>
2025-07-16 23:30:03 -0500

    feat(auth): add controller and endpoint for separate company creation

--- Backend/anecacao_api/src/main/java/com/anecacao/api/auth/controller/UserRestController.java
+++ Backend/anecacao_api/src/main/java/com/anecacao/api/auth/controller/UserRestController.java
@@ -14,8 +14,15 @@ public class UserRestController {
     private final UserService userService;
 
     @PostMapping("/auth/register")
-    public ResponseEntity<UserRegistrationResponseDTO> registerUser(@RequestBody @Valid UserRegistrationRequestDTO user){
+    public ResponseEntity<UserRegistrationResponseDTO> registerUser(
+            @RequestBody @Valid UserRegistrationRequestDTO user){
         return new ResponseEntity<>(userService.registerUser(user), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/auth/profile-setup")
+    public void completeUserInfo(
+            @RequestBody @Valid UserProfileSetUpRequestDTO user){
+        userService.completeUserInfo(user);
     }
 
     @PostMapping("/auth/login")
