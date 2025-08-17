fc36d08be36072ccb9ff3d121c6c54c61d8999d6
isaías <112284541+isaiasgh@users.noreply.github.com>
2025-08-17 14:38:38 -0500

    fix(user-roles)

--- Backend/anecacao_api/src/main/java/com/anecacao/api/auth/domain/service/impl/UserServiceImpl.java
+++ Backend/anecacao_api/src/main/java/com/anecacao/api/auth/domain/service/impl/UserServiceImpl.java
@@ -137,6 +137,7 @@ public class UserServiceImpl implements UserService {
         Role role = roleRepository.findByName(userUpdateRoleDTO.getRole())
                 .orElseThrow(() -> new RuntimeException("Role not found."));
 
+        user.getRoles().removeAll();
         user.getRoles().add(role);
         userRepository.save(user);
     }
